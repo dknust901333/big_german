5f5fb8c265feac605c51ee7a256318b538b49088
Adam Knust <aknust@demandjump.com>
2020-02-26 21:29:29 -0600

    card game setup

---
+++ pycards/app.py
@@ -0,0 +1,9 @@
+from pycards.cards.cardsets import euchre_cards, poker_cards
+
+
+def run():
+    print('Euchre Cards:')
+    print(euchre_cards)
+    print
+    print('Poker Cards:')
+    print(poker_cards)
